a0e349df621061c0bb3a1fb0f1e762411f7e56d4
Tom Long <mytomlong@gmail.com>
2017-01-02 01:23:06 +0800

    2017.1.2
    Contribution Everyday!

--- ADT/main.c
+++ ADT/main.c
@@ -6,13 +6,13 @@
  ************************************************************************/
 
 #include<stdio.h>
-
+#include<stdlib.h>
 struct node;
 typedef int Elemtype;
 typedef struct node *PtrNode;
 typedef PtrNode Position;
 typedef PtrNode List;
-
+#define SIZE sizeof(struct node) 
 
 struct node{
     Elemtype Data;
@@ -21,3 +21,45 @@ struct node{
 int IsEmpty(List L){
     return L->Next==NULL;
 }
+
+int IsLast(Position X, List L){
+    return X->Next == NULL;
+}
+
+Position Find(Elemtype X,List L){
+    Position P;
+    P = L->Next;
+    while(P!=NULL && P->Data!=X)
+        P = P->Next;
+    return P;
+}
+
+Position FindPrevious(Elemtype X, List L){
+    Position P = L;
+    while(P->Next!=NULL && P->Next->Data!= X)
+        P = P->Next;
+    return P;
+}
+
+void Delete(Elemtype X, List L){
+    Position TemCell,P;
+    P = FindPrevious(X,L);
+    if(IsLast(P,L)){
+        TemCell = P->Next;
+        TemCell->Next = TemCell->Next;
+        free(TemCell);
+    }
+}
+
+void Insert(Elemtype X, List L, Position P){
+    Position TemCell;
+    TemCell = malloc(SIZE);
+    if(TemCell==NULL)
+        FatalError("Out Of Space!");
+    TemCell->Data = X;
+    P->Next = TemCell;
+    TemCell->Next = P->Next;
+}
+
+
+
