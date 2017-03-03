85c2ae030d661e475eefa16c6cd06cf1a0665649
Tom Long <mytomlong@gmail.com>
2017-03-03 12:50:11 +0800

    Add MSS1

--- MOOC/MaxSubSum.c
+++ MOOC/MaxSubSum.c
@@ -6,14 +6,17 @@ clock_t start,end;
 double mytime;
 unsigned long K=200;
 int MSS(int Arr[],int Num);
+int MSS1(int Arr[],int Num,int *head,int *tail);
 double TimeTest(int (*ptr)(),unsigned long MAX);
 
 int main()
 {
-  //  int Arr[6]={-2,11,-4,13,-5,-2};
+    int Arr[6]={-3,11,-4,13,-5,-2};
+    int head,tail;
     //printf("%lu\n",clock()/CLOCKS_PER_SEC);
-    printf("test\n");
-    printf("%f",TimeTest(MSS,K));
+   // printf("test\n");
+    printf("%d\t%d\t%d",MSS1(Arr,6,&head,&tail),head,tail);
+   // printf("%f",TimeTest(MSS,K));
     return 0;
 }
 
@@ -43,3 +46,39 @@ int MSS(int Arr[],int Num)
     }
     return MaxSum;
 }
+
+
+int MSS1(int Arr[],int Num,int *head,int *tail)
+{
+    int ThisSum=0, MaxSum=0;
+    for(int i=0;i<Num;++i)
+    {
+        ThisSum += Arr[i];
+        if(ThisSum<0)
+        {
+            if(i==(Num-1))
+            {
+                *head = Arr[0];
+                return 0;
+            }
+            else
+            {
+            ThisSum = 0;
+            *head = Arr[i+1];
+            *tail = *head;
+            }
+        }
+        else if(ThisSum>MaxSum)
+        {
+            MaxSum = ThisSum;
+            *tail = Arr[i];
+        }
+    }
+    return MaxSum;
+}
+
+
+
+
+
+
