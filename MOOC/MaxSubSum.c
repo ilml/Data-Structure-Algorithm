fdaae4690124b9afc724590716a54627b3a5ab32
Tom Long <mytomlong@gmail.com>
2017-03-15 22:13:55 +0800

    Debug

--- MOOC/MaxSubSum.c
+++ MOOC/MaxSubSum.c
@@ -4,18 +4,30 @@
 
 clock_t start,end;
 double mytime;
-unsigned long K=200;
+unsigned long K=20;
 int MSS(int Arr[],int Num);
 int MSS1(int Arr[],int Num,int *head,int *tail);
 double TimeTest(int (*ptr)(),unsigned long MAX);
 
 int main()
 {
-    int Arr[6]={-3,11,-4,13,-5,-2};
+    int num;
+//    int Arr1[6]={-10,-10,17,-21,14,12};
+//    int Arr1[10]={-10, 1 ,2, 3 ,4 ,5 ,-23 ,3 ,7 ,-21};
     int head,tail;
     //printf("%lu\n",clock()/CLOCKS_PER_SEC);
-   // printf("test\n");
-    printf("%d\t%d\t%d",MSS1(Arr,6,&head,&tail),head,tail);
+//    printf("test\n");
+//    scanf("%d",&num);
+ //   for(int i=0;i<num;++i)
+  //      scanf("%d",&Arr[i]);
+
+	scanf("%d",&num);
+    int Arr[num];
+    for(int i=0;i<num;++i)
+		scanf("%d",&Arr[i]);
+
+//    printf("%d\n",MSS(Arr,num));
+    printf("%d %d %d",MSS1(Arr,num,&head,&tail),head,tail);
    // printf("%f",TimeTest(MSS,K));
     return 0;
 }
@@ -50,33 +62,34 @@ int MSS(int Arr[],int Num)
 
 int MSS1(int Arr[],int Num,int *head,int *tail)
 {
-    int ThisSum=0, MaxSum=0;
-    for(int i=0;i<Num;++i)
+   int ThisSum=0, MaxSum=0;
+   int thishead,thistail;
+   thishead = Arr[0];
+   for(int i=0;i<Num;++i)
     {
         ThisSum += Arr[i];
+        thistail = Arr[i];
         if(ThisSum<0)
         {
-            if(i==(Num-1))
-            {
-                *head = Arr[0];
-                return 0;
-            }
-            else
-            {
             ThisSum = 0;
-            *head = Arr[i+1];
-            *tail = *head;
-            }
+            if(i<(Num-1))
+                thishead = Arr[i+1];
+            thistail = thishead;
         }
         else if(ThisSum>MaxSum)
         {
             MaxSum = ThisSum;
-            *tail = Arr[i];
+            *tail = thistail;
+            *head = thishead;
         }
+    }
+    if(MaxSum==0)
+    {
+        *head = Arr[0];
+        *tail = Arr[Num-1];
     }
     return MaxSum;
 }
-
 
 
 
